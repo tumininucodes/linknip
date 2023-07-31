e07ded170db939695400e59e7d89b92ffa14e5bf
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-07-31 03:44:02 +0100

    development

---
+++ internal/helpers/helpers.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"errors"
+	"math"
+	"strings"
+)
+
+
+const (
+	alphabet      = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	largestuint64 = 18446744073709551615
+)
+
+func Base62Decode(encodedString string) (uint64, error) {
+	var number uint64
+	length := len(alphabet)
+
+	for i, symbol := range encodedString {
+		alphabeticPosition := strings.IndexRune(alphabet, symbol)
+		if alphabeticPosition == -1 {
+			return uint64(alphabeticPosition), errors.New("cannot find symbol in alphabet")
+		}
+		number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
+	}
+
+	return number, nil
+}
